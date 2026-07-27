6da440dc994f8905300c16a0731cbe68142b0771
FarhanAliCs <farhanoff12@gmail.com>
2026-07-27 16:01:27 +0500

    Complete day 10 : Modules and OOP Basics

---
+++ day 10 Modules, Packages and Object-Oriented Programing (Introduction)/Math_operation.py
@@ -0,0 +1,25 @@
+def addition(num1 , num2):
+    return num1+num2
+
+#subtraction
+def subtraction(num1 , num2):
+    return num1-num2
+
+#Multiplication
+def multiplication(num1,num2):
+    return num1*num2
+
+#Division
+def division(num1,num2):
+    if num2 == 0:
+       print("Not divisble by zero .")
+    return num1/num2
+
+#Power
+def power(num1 , num2 ):
+    return num1**num2
+
+#squre Root
+def squre_Root(num1 ):
+    return num1**0.5
+
