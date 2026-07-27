39607e23f0b41a639633cdaedd008ae7be8266c8
FarhanAliCs <farhanoff12@gmail.com>
2026-07-27 15:57:03 +0500

    -

--- day 10 Modules, Packages and Object-Oriented Programing (Introduction)/Module practice , Main.py
+++
@@ -1,9 +0,0 @@
-import Math_operation as operation
-
-add=operation.addition(12,13)
-print(add)
-subtract=operation.subtraction(12,1)
-print(subtract)
-multiplication=operation.multiplication(12,14)
-print(multiplication)
-
