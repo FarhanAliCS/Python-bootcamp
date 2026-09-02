070a42b0dc10b59fcd9f3ea0852cbdb9d48600ef
FarhanAliCs <farhanoff12@gmail.com>
2026-09-02 16:27:45 +0500

    Added some tasks in  day 25

--- Day 25 - Pathlib/Practice/prac 1 - from pathlab import path and create file.py.py
+++ Day 25 - Pathlib/Practice/prac 1 - from pathlab import path and create file.py.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 
 folder=Path("Day 25 - pathlab")
+
 if folder.exists():
+
     file=Path(folder)/"Practice "
+    
     file.mkdir(exist_ok=True)
     
--- Day 25 - Pathlib/Practice/prac 3 - Find only txt files .py
+++ Day 25 - Pathlib/Practice/prac 3 - Find only txt files .py
@@ -1,9 +1,13 @@
 from pathlib import Path
 
 folder=Path("Practice")
+
 if folder.exists():
     file=folder.glob("*.txt")
+
     for f in file:
         print(f)
+
 else:
+    
     print("Folder not exists .")
